26fb916ac081cca989e7641226ea74fba1600052
RXHAP <rxhap1009@gmail.com>
2024-09-11 22:11:15 +0900

    MySQLのテスト

--- vote/vote_check.py
+++ vote/vote_check.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return '200'
 
-@app.route("/test")
-def test():
-  return "test"
+@app.route("/<id>")
+def id_check(id):
+  conn = mysql.connector.connect(
+    host="localhost",
+    user="rxhap_2024",
+    password="kmarisa1009",
+    database="rxhap_vote2024"
+  )
+  cursor = conn.cursor()
+  cursor.execute("select * from vote_check where id="+id+";")
+  result = cursor.fetchall()
+
+  cursor.close()
+  conn.close()
+  return result
